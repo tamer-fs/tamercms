d986d58ece1112f1ce09c277f17fda46f6a44493
tamer-fs <113843535+tamer-fs@users.noreply.github.com>
2024-04-05 18:35:49 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import TitleBar from "./components/TitleBar";
+import $ from "jquery";
+import "./App.css";
+import { useState } from "react";
+import db from "./firebaseConfig";
+import { onValue, ref, set } from "firebase/database";
 
 function App() {
+  const [buisnessName, setBuisnessName] = useState("");
+  const [password, setPassword] = useState("");
+  const [inputFields, setInputFields] = useState([]);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [usedBuisnessName, setUsedBuisnessName] = useState("");
+
+  const handleLoginSubmit = (e) => {
+    let buisnessData;
+    e.preventDefault();
+    const buisnessRef = ref(db, `${buisnessName}/`);
+    onValue(buisnessRef, (snapshot) => {
+      buisnessData = snapshot.val();
+    });
+    if (buisnessData != null) {
+      // buisness exists
+      setUsedBuisnessName(buisnessName);
+      const passwordRef = ref(db, `${buisnessName}/Password`);
+      onValue(passwordRef, (snapshot) => {
+        console.log(snapshot.val(), password);
+        if (snapshot.val() == password) {
+          // correct password
+          loadData();
+        } else {
+          alert("verkeerd wachtwoord / bedrijfsnaam");
+        }
+      });
+    } else {
+      alert("verkeerd wachtwoord / bedrijfsnaam");
+    }
+  };
+
+  const loadData = () => {
+    const inputsRef = ref(db, `${buisnessName}/Inputs`);
+    onValue(inputsRef, (snapshot) => {
+      setInputFields(snapshot.val());
+      console.log(snapshot.val());
+    });
+    setLoggedIn(true);
+    console.log(inputFields);
+  };
+
+  const handleDataChange = (id, data, value) => {
+    set(ref(db, `${usedBuisnessName}/Inputs/${id}`), {
+      for: data.for,
+      content: value,
+    });
+    loadData();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <TitleBar />
+      <div className="form-container">
+        <form
+          className="main-form"
+          action="http://localhost:8000/server.php"
+          method="post"
+          onSubmit={(e) => handleLoginSubmit(e)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="mb-3">
+            <label htmlFor="buisnessInput" className="form-label">
+              Bedrijf naam
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="buisnessInput"
+              value={buisnessName}
+              name="buisnessName"
+              onChange={(e) => setBuisnessName(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="passwordInput" className="form-label">
+              Password
+            </label>
+            <input
+              type="wachtwoord"
+              className="form-control"
+              name="password"
+              id="passwordInput"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Log in
+          </button>
+        </form>
+        <div className="cms-container">
+          {inputFields.map((data, index) => (
+            <div className="input-container">
+              <label htmlFor={data.for}>{data.for}</label>
+              <textarea
+                id={data.for}
+                className="form-control"
+                placeholder={data.content}
+              />
+              <button
+                className="btn btn-outline-primary"
+                onClick={(e) =>
+                  handleDataChange(
+                    index,
+                    data,
+                    document.getElementById(data.for).value
+                  )
+                }
+              >
+                Invoeren
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/TitleBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TitleBar.css";
+
+function TitleBar() {
+  return (
+    <nav>
+      <h1>Tamer CMS</h1>
+    </nav>
+  );
+}
+
+export default TitleBar;
